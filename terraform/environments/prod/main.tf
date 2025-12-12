d3ebfae09b683563105f06b97a786f20cae05da5
binbin <gunchou02@gmail.com>
2025-12-12 23:06:22 +0900

    Update main.tf

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -39,6 +39,7 @@ module "eks" {
 resource "aws_ecr_repository" "app" {
   name                 = "python-web-app"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
